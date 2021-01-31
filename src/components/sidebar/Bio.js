744ea97aee1bd14ba3d28bdbefbbdaa57a6d537f
SeanOh <ohsean.dev@gmail.com>
2021-01-31 22:33:35 +0900

    profile 사진 변경

--- src/components/sidebar/Bio.js
+++ src/components/sidebar/Bio.js
@@ -1,7 +1,7 @@
 import React from "react"
 import "./sidebar.css"
 
-import willjw3 from "../../images/willjw3.jpg"
+import willjw3 from "../../images/sean.jpg"
 
 const Bio = ({ author, tagline }) => {
 
